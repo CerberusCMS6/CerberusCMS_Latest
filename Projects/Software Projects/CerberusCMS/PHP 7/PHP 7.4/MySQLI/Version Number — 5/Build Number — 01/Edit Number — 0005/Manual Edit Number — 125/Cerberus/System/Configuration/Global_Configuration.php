4df2533537cf5cc0fb44214e588165c9e20b3cab
Cerberus Content Management System <CerberusCMS6@Proton.me>
2025-06-23 09:28:30 -0700

    Add files via upload

--- Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQLI/Version Number — 5/Build Number — 01/Edit Number — 0005/Manual Edit Number — 125/Cerberus/System/Configuration/Global_Configuration.php
+++ Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQLI/Version Number — 5/Build Number — 01/Edit Number — 0005/Manual Edit Number — 125/Cerberus/System/Configuration/Global_Configuration.php
@@ -525,7 +525,7 @@ $_PROJECT_VERSION_FILE_NUMBER						= "0.7.7";
  ============================================================================================================
 */
 
-$_PROJECT_STRING_LEGAL_COPYRIGHT					= "&copy; G.C.'s Blue Raven Feather S.P. and Software, G.C. Johnson Works 2000 - Present-Day";
+$_PROJECT_STRING_LEGAL_COPYRIGHT					= "&copy; G.C.'s Immolated Sword, Feather and Stone Software; G.C. Johnson Works 2000 - Present-Day";
 $_PROJECT_STRING_LEGAL_ELECTRONIC_MAIL_ADDRESS				= "CerberusCMS6@Proton.me";
 $_PROJECT_STRING_LEGAL_NAME_AUTHOR					= "G.C. Johnson";
 $_PROJECT_STRING_NAME_EXTENDED						= "Cerberus Content Management System";
@@ -573,8 +573,8 @@ $_PROJECT_WEB_SERVER_TECHNOLOGY_VERSION_PHP_ZEND			= zend_version();
  ============================================================================================================
 */
 
-$_PROJECT_DEVELOPER_STRING_NAME_SHORT					= "G.C.'s Blue Raven Feather";
-$_PROJECT_DEVELOPER_STRING_NAME_EXTENDED				= "G.C.'s Blue Raven Feather, S.P. and Software";
+$_PROJECT_DEVELOPER_STRING_NAME_SHORT					= "G.C.'s Immolated Sword, Feather and Stone Software";
+$_PROJECT_DEVELOPER_STRING_NAME_EXTENDED				= "G.C.'s Immolated Sword, Feather and Stone Software";
 
 /*
  ============================================================================================================
